51e72e06e521409d6d4a41af71d59035ff633f8c
DanielKusumaPratama <89924013+DanielKusumaPratama@users.noreply.github.com>
2022-03-21 18:08:18 +0700

    data

--- Data.py
+++ Data.py
@@ -1,44 +1,68 @@
-import os.path
+import os
 import json
-
-datasaatini = [["5191011008", "5191011024", "5191011029", "5191011036", "5191011038"],
-        ["Mayaltn417#", "220222", "032001", "asdqwe123@@A", "032001"]]
-# password =
-
-print(datasaatini[0][1])
-print(datasaatini[1][1])
-path = "Preset"
-nama_file = "a.json"
-full_path = path+ "/" +nama_file
-cek_file = os.path.exists(full_path)
-print(full_path)
 
 class data():
 
     def __init__(self):
         super().__init__()
+        self.PATH_PRESET = "Preset/"
 
-    def datatojson (self):
-        if cek_file == False:
-            # data = {'mahasiswa': [{'NIM': data1[0][0], 'Password': data1[1][0]}]}
-            self.file(datasaatini)
-        else:
-            self.file_tidak_ada(self.tambah_data(datasaatini))
+    def listPreset(self):
+        return os.listdir(self.PATH_PRESET)
 
-    def file_tidak_ada(self, data):
-        with open(full_path, 'w') as outfile:
-            for i in range(len(data)):
-                data = {'mahasiswa': [{'NIM': data[0][i], 'Password': data[1][i]}]}
-                json.dump(data, outfile, indent=3)
+    def getData(self, file):
+        with open(self.PATH_PRESET+file) as openfile:
+            preset = json.load(openfile)
+        return preset
+    
+    def simpanJson(self, file_name, data_json):
+        with open(self.PATH_PRESET + file_name, "w") as outfile:
+            json.dump(data_json, outfile)
 
-    def tambah_data(self, data_baru):
-        with open(full_path, 'r+') as read:
-            bacadata = json.load(read)
-            # databaru = {'NIM': data1[0][0], 'Password': data1[1][0]}
-            bacadata["mahasiswa"].append(data_baru)
-            print(bacadata)
-            return bacadata
 
-aa = data()
-aa.datatojson()
+    # def load_preset(self, file):
+
+
+# datasaatini = [["5191011008", "5191011024", "5191011029", "5191011036", "5191011038"],
+#         ["Mayaltn417#", "220222", "032001", "asdqwe123@@A", "032001"]]
+# # password =
+
+# print(datasaatini[0][1])
+# print(datasaatini[1][1])
+# path = "Preset"
+# nama_file = "a.json"
+# full_path = path+ "/" +nama_file
+# cek_file = os.path.exists(full_path)
+# print(full_path)
+# print(os.listdir("Preset/"))
+
+# class data():
+
+#     def __init__(self):
+#         super().__init__()
+#         self.PATH_PRESET = "Preset/"
+
+#     def datatojson (self):
+#         if cek_file == False:
+#             # data = {'mahasiswa': [{'NIM': data1[0][0], 'Password': data1[1][0]}]}
+#             self.file(datasaatini)
+#         else:
+#             self.file_tidak_ada(self.tambah_data(datasaatini))
+
+#     def file_tidak_ada(self, data):
+#         with open(full_path, 'w') as outfile:
+#             for i in range(len(data)):
+#                 data = {'mahasiswa': [{'NIM': data[0][i], 'Password': data[1][i]}]}
+#                 json.dump(data, outfile, indent=3)
+
+#     def tambah_data(self, data_baru):
+#         with open(full_path, 'r+') as read:
+#             bacadata = json.load(read)
+#             # databaru = {'NIM': data1[0][0], 'Password': data1[1][0]}
+#             bacadata["mahasiswa"].append(data_baru)
+#             print(bacadata)
+#             return bacadata
+
+# aa = data()
+# aa.datatojson()
 
--- GUI.py
+++ GUI.py
@@ -1,7 +1,7 @@
 from threading import *
 from PyQt5 import QtWidgets, uic
-import sys
-import presensi
+import sys, os, random
+import presensi, Data
 from PyQt5.QtWidgets import QTableWidgetItem, QListWidgetItem
 
 import json
@@ -12,24 +12,67 @@ class Ui(QtWidgets.QMainWindow):
         uic.loadUi('presensi.ui', self)
 
         self.presensi = presensi.presensi()
+        self.data = Data.data()
         self.dataui_NIM = []
         self.dataui_Password = []
+        self.is_new_preset = True
         self.show()
         self.loadkeui()
         self.pushButton_Presensi.clicked.connect(self.thread_presensi)
         self.pushButton_Tambah.clicked.connect(self.tambahrow)
         self.pushButton_Hapus.clicked.connect(self.hapusrow)
+        self.check_preset()
+        self.pushButton_LoadPreset.clicked.connect(self.load_preset)
+        self.pushButton_NewPreset.clicked.connect(self.new_preset)
+        self.pushButton_Simpan.clicked.connect(self.simpan_json)
+
+    def simpan_json(self):
+        if self.is_new_preset == True:
+            file_name = "data" + str(random.randint(1,1000)) + ".json"
+        else:
+            file_name = self.comboBox_Preset.currentText()
+        data_json = {}
+        for row in range(self.Tabel_Mahasiswa.rowCount()):
+            nim =self.Tabel_Mahasiswa.item(row,0).text()
+            password = self.Tabel_Mahasiswa.item(row,1).text()
+            data_json[row] = {"nim":nim, "password": password}
+        self.data.simpanJson(file_name, data_json)
+        self.Label_Presensi_2.setText(f"Daftar Mahasiswa: {file_name}")
+        self.check_preset()
+
+    def new_preset(self):
+        self.Tabel_Mahasiswa.setRowCount(0)
+        self.Label_Presensi_2.setText("Daftar Mahasiswa: New Preset")
+        self.is_new_preset = True
+
+    def load_preset(self):
+        preset = self.comboBox_Preset.currentText()
+        self.new_preset()
+        self.Label_Presensi_2.setText(f"Daftar Mahasiswa: {preset}")
+        user_data = self.data.getData(preset)
+        self.Tabel_Mahasiswa.setRowCount(len(user_data.keys()))
+        for index in user_data:
+            self.Tabel_Mahasiswa.setItem(int(index), 0, QTableWidgetItem(user_data[index]["nim"]))
+            self.Tabel_Mahasiswa.setItem(int(index), 1, QTableWidgetItem(user_data[index]["password"]))
+        self.is_new_preset = False
+
+    def check_preset(self):
+        # list_file = os.listdir(self.PATH_PRESET)
+        self.comboBox_Preset.clear()
+        list_file = self.data.listPreset()
+        if len(list_file) > 0:
+            self.comboBox_Preset.addItems(list_file)
 
     def loadkeui(self):
-
-        email = self.presensi.email()
-        password = self.presensi.password()
-        total = len(email)
-        print("data masuk ke UI: ", total)
-        self.Tabel_Mahasiswa.setRowCount(total)
-        for i in range(total):
-            self.Tabel_Mahasiswa.setItem(i, 0, QTableWidgetItem(str(email[i])))
-            self.Tabel_Mahasiswa.setItem(i, 1, QTableWidgetItem(str(password[i])))
+        return
+        # email = self.presensi.email()
+        # password = self.presensi.password()
+        # total = len(email)
+        # print("data masuk ke UI: ", total)
+        # self.Tabel_Mahasiswa.setRowCount(total)
+        # for i in range(total):
+        #     self.Tabel_Mahasiswa.setItem(i, 0, QTableWidgetItem(str(email[i])))
+        #     self.Tabel_Mahasiswa.setItem(i, 1, QTableWidgetItem(str(password[i])))
 
     def loadkeprogram(self):
         row = self.Tabel_Mahasiswa.rowCount()
